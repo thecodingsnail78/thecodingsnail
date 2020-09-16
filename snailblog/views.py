1f42b7358643161aa06434f6b0432501bab6d883
Gail Bartlett <gb.at.home@gmail.com>
2020-09-16 17:40:05 +0100

    Added login/logout function

--- snailblog/views.py
+++ snailblog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
@@ -13,6 +14,7 @@ def post_detail(request,pk):
     post=get_object_or_404(Post,pk=pk)
     return render(request,'snailblog/post_detail.html', {'post':post})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -27,6 +29,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'snailblog/post_edit.html',{'form':form})
 
+@login_required
 def post_edit(request,pk):
     post = get_object_or_404(Post,pk=pk)
     if request.method == "POST":
@@ -41,15 +44,18 @@ def post_edit(request,pk):
         form=PostForm(instance=post)
     return render(request, 'snailblog/post_edit.html',{'form':form})
 
+@login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'snailblog/post_draft_list.html', {'posts': posts})
 
+@login_required
 def post_publish(request,pk):
     post=get_object_or_404(Post,pk=pk)
     post.publish()
     return redirect('post_detail',pk=pk)
-
+    
+@login_required
 def post_remove(request,pk):
     post = get_object_or_404(Post,pk=pk)
     post.delete()
