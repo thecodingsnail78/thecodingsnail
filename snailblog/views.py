33a0cb961b408d710198f6e2320d6d3ae2750600
Gail Bartlett <gb.at.home@gmail.com>
2020-10-26 17:46:18 +0000

    new template commit

--- snailblog/views.py
+++ snailblog/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Post
-from .forms import PostForm
+from .models import Post, Comment
+from .forms import PostForm,CommentForm
 
 # Create your views here.
+
+def index(request):
+    return render(request,'snailblog/index.html')
 
 def post_list(request):
     posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -54,10 +57,34 @@ def post_publish(request,pk):
     post=get_object_or_404(Post,pk=pk)
     post.publish()
     return redirect('post_detail',pk=pk)
-    
+
 @login_required
 def post_remove(request,pk):
     post = get_object_or_404(Post,pk=pk)
     post.delete()
     return redirect('post_list')
 
+def add_comment_to_post(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'snailblog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
